63dda11c27736654f61a4197dba8833cc7347e2a
Mashudu Malukwe <86242460+mashudu2021@users.noreply.github.com>
2024-08-09 21:41:20 +0200

    Add files via upload

---
+++ web crawler/spider.js
@@ -0,0 +1,13 @@
+spiderJson = {"nodes":[
+{"weight":2,"rank":19.0, "id":1, "url":"http://www.dr-chuck.com"},
+{"weight":1,"rank":11.400000000000036, "id":2, "url":"http://www.dr-chuck.com/dr-chuck/resume/index.htm"},
+{"weight":1,"rank":11.400000000000036, "id":4, "url":"http://www.dr-chuck.com/sakai-book"},
+{"weight":1,"rank":10.450000000000015, "id":10, "url":"http://www.dr-chuck.com/dr-chuck/resume/leadership.htm"},
+{"weight":1,"rank":0.0, "id":13, "url":"http://www.dr-chuck.com/dr-chuck/resume"}],
+"links":[
+{"source":0,"target":1,"value":3},
+{"source":0,"target":2,"value":3},
+{"source":1,"target":0,"value":3},
+{"source":1,"target":3,"value":3},
+{"source":4,"target":0,"value":3},
+{"source":4,"target":4,"value":3}]};
